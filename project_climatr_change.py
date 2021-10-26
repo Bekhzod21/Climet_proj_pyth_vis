a00db4d45c881a4428fc3d9f294f1f1eb299153d
Bekhzod Odilov <behzododilov97@gmail.com>
2021-10-27 01:11:45 +0500

    fh

--- project_climatr_change.py
+++ project_climatr_change.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats
+import seaborn as sns
+import matplotlib as plt
+
 
 print("REALLY?")
 # news
@@ -21,7 +25,12 @@ print("k")
 print("done?")
 
 df = pd.read_csv(r'C:\Users\Behzod\Documents\python\python tutorials_QY\python project climate\Climet_proj_pyth_vis-1\Environment_Temperature_change_E_All_Data_Normalized.csv', encoding="latin1")
-# print(df)
-df.shape
-df.sum()
+print(df)
+
+df.describe()
 print('d')
+
+df[['Area', 'Value']].groupby(['Area']).mean().sort_values('Value')
+#plt.figure(figsize=(9, 4))
+sns.lineplot(x="Year", y="Value", data=df)
+plt.show()
